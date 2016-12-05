98053f403b1b44dad121e02bb14dd82129ea6ee5
AsqiyahTamara <asqiyahtamara27@gmail.com>
2016-12-05 14:47:47 +0700

    upload manajemen anggota

---
+++ e_koperasi/application/views/halaman_admin.php
@@ -0,0 +1,28 @@
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    
+    <!-- Main content -->
+    <section class="content">
+      <!-- Main row -->
+      <div class="row">
+        <!-- right col (We are only adding the ID to make the widgets sortable)-->
+        <section class="col-lg-12">
+          
+                <div class="col-lg-12">
+                    <div class="alert alert-success">
+                        </i><b>&nbsp;Hello! </b>Welcome Back <b> <?php echo $this->session->userdata('nama_admin'); ?> </b>
+                        <i class="fa  fa-pencil"></i><b> di E-Koperasi
+                    </div>
+                </div>
+                
+                    
+                   
+                </div>
+            
+        </section>
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
